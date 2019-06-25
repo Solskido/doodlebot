669ae6e0d239142d6a6fa01b76c1c772270645d1
Max Stahl <solskido@gmail.com>
2019-06-25 11:47:54 -0400

    Pick one

--- app.js
+++ app.js
@@ -62,7 +62,38 @@ client.on("message", (message) => {
   }
 
   console.log(`Incoming message: ${message.content}`);
-  if(internals.isConchCommand(message)) {
+  if(internals.isChooseCommand(message)) {
+    const parsedChooseCommand = internals.parseChooseCommand(message);
+    if((parsedChooseCommand.selectionCount >= parsedChooseCommand.choices.length)
+    || (parsedChooseCommand.selectionCount >= _.uniq(parsedChooseCommand.choices).length)) {
+      message.channel.send({
+        "embed": {
+          "color": 11699390,
+          "title": "The magic conch says...",
+          "description": "...really?"
+        }
+      });
+
+      return;
+    }
+
+    let picks = [];
+    let i;
+    for(i = 0; i < parsedChooseCommand.selectionCount; i++) {
+      const pick = _.sample(parsedChooseCommand.choices);
+      picks.push(pick);
+      parsedChooseCommand.choices = _.without(parsedChooseCommand, pick);
+    }
+
+    message.channel.send({
+      "embed": {
+        "color": 11699390,
+        "title": "The magic conch says...",
+        "description": picks.join(", ")
+      }
+    });
+  }
+  else if(internals.isConchCommand(message)) {
     message.channel.send({
       "embed": {
         "color": 11699390,
@@ -115,6 +146,106 @@ client.on("message", (message) => {
 
 internals.isConchCommand = (message) => {
   return (message.content.substr(0, 11).toLowerCase() === "magic conch");
+};
+
+internals.isChooseCommand = (message) => {
+  const parsedChooseCommand = internals.parseChooseCommand(message);
+  return ((parsedChooseCommand.selectionCount > 0)
+       && (parsedChooseCommand.choices.length > 0));
+};
+
+internals.parseChooseCommand = (message) => {
+  if(message.content.substr(0, 11).toLowerCase() !== "magic conch") {
+    return {
+      "selectionCount": 0
+    };
+  }
+
+  const words = _.words(message.content);
+  const pickAt = internals.getHotWordIndexes(words, ["pick"]);
+  if(_.isEmpty(pickAt)) {
+    return {
+      "selectionCount": 0
+    };
+  }
+
+  let selection = 0;
+  _.forEach(pickAt, (pickAtIndex) => {
+    if(!selection
+    && (pickAtIndex < words.length)) {
+      // This isn't the last word in the message
+      const number = internals.wordToNumber(words[pickAtIndex + 1]);
+      if(number > 0) {
+        selection = internals.wordToNumber(words[pickAtIndex + 1]);
+      }
+    }
+  });
+
+  const choices = [];
+  const split = message.content.split(",");
+  _.forEach(split, (csv, i) => {
+    if(csv.substr(0, 11).toLowerCase() === "magic conch") {
+      csv = csv.substr(11).trim();
+    }
+
+    if(csv.substr(0, 4) === "pick") {
+      csv = _.tail(_.compact(csv.substr(4).split(" "))).join(" ");
+    }
+
+    choices.push(csv.trim());
+  });
+
+  return {
+    "choices": choices,
+    "selectionCount": selection
+  };
+};
+
+internals.NUMBER_WORDS = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten"
+];
+internals.wordToNumber = (word) => {
+  let number = 0;
+  try {
+    number = parseInt(word, 10);
+  }
+  catch(e) {
+    number = 0;
+  }
+
+  if((number !== 0)
+  && !_.isNaN(number)) {
+    return number;
+  }
+
+  number = _.indexOf(internals.NUMBER_WORDS, word);
+
+  return Math.max(0, number);
+};
+
+internals.getHotWordIndexes = (words, hotWords) => {
+  const indexes = [];
+  _.forEach(words, (word, i) => {
+    const matches = _.findIndex(hotWords, (hotWord) => {
+      return (hotWord.toLowerCase() === word.toLowerCase());
+    });
+
+    if(matches >= 0) {
+      indexes.push(i);
+    }
+  });
+
+  return indexes;
 };
 
 internals.isMe = (string) => {
